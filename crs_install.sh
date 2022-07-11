f0be4f42c6d7f2d972b59ecd4dac6f980a567af8
DUNYU LIU <dunyuliu@gmail.com>
2022-07-11 18:47:25 -0500

    Update crs_install.sh
    Add default CONDA_PREFIX

--- crs_install.sh
+++ crs_install.sh
@@ -1,4 +1,6 @@
+echo $CONDA_PREFIX
 cd Release
-gsl_include=/home/dliu/anaconda3/envs/gsl/include/
+gsl_include=$CONDA_PREFIX/include/
+gsl_lib=$CONDA_PREFIX/lib/
 make clean
-make all gsl_include=$gsl_include
+make all gsl_include=$gsl_include gsl_lib=$gsl_lib
