208a50aff9786ff9a96af2c4df806fff51b9185d
DUNYU LIU <dunyuliu@gmail.com>
2022-07-12 17:09:19 -0500

    Update test example and gsl_lib path
    Add LD_LIBRARY_PATH to gsl_lib

--- crs_install.sh
+++ crs_install.sh
@@ -2,6 +2,8 @@ echo $CONDA_PREFIX
 cd Release
 gsl_include=$CONDA_PREFIX/include/
 gsl_lib=$CONDA_PREFIX/lib/
+export LD_LIBRARY_PATH=$gsl_lib
+
 make clean
 make all gsl_include=$gsl_include gsl_lib=$gsl_lib
 
